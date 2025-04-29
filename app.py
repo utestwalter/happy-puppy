92aa27bbf0cf49b47656dcf6e034ec7d57bcd2a3
Elena Walter <elenawalter@Elenas-iMac.attlocal.net>
2025-04-29 14:00:10 -0400

    Set confidence threshold to 0.999

--- app.py
+++ app.py
@@ -62,7 +62,7 @@ def index():
                 predicted_class = class_names[np.argmax(pred)]
                 
 
-                threshold = 0.998
+                threshold = 0.999
 
                 if confidence >= threshold:
                     prediction = f"Breed: {predicted_class} ({confidence*100:.2f}%)"
